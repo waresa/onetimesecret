0818f3028d9ba8e2cba9b61f11a7ad71f08f7ace
Wares Amiry <wares.amiry@schibsted.com@A121384-NO.local>
2023-08-25 14:31:07 +0200

    added everything

--- redis.php
+++ redis.php
@@ -8,16 +8,14 @@
 require_once 'vendor/autoload.php';
 
 use Predis\Client;
+$redisPassword = 'xuvW6m2U0866ZHTx9a0CqB60OEB5oXrK';
 
 // Connect to Redis server
 $redis = new Client([
     'scheme' => 'tcp',
     'host' => 'redis-14030.c2.eu-west-1-3.ec2.cloud.redislabs.com',
     'port' => 14030,
-    'password' => 'xuvW6m2U0866ZHTx9a0CqB60OEB5oXrK',
+    'password' => $redisPassword,
 ]);
-$redis->connect($redisHost, $redisPort);
-if (!empty($redisPassword)) {
-    $redis->auth($redisPassword);
-}
+
 ?>
